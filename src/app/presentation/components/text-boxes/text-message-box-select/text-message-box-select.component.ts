162a99fb08927cee98c7e19375ea1473a2e6891a
Carlos Ramirez <carlos@extrasistemas.com>
2024-02-12 23:33:23 -0600

    feat: added component for select and interfaces for messages

---
+++ src/app/presentation/components/text-boxes/text-message-box-select/text-message-box-select.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+export interface Options {
+ id: string;
+  name: string;
+}
+export interface TextMessageBoxEvent {
+  prompt?: string | null;
+  selectedOption: String;
+}
+
+@Component({
+  selector: 'app-text-message-box-select',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './text-message-box-select.component.html',
+})
+export class TextMessageBoxSelectComponent {
+  @Input() public plahaceholder: string =''
+  @Input() public disabledCorrection: boolean = false;
+  @Input({required: true}) public options!: Options[]
+  @Output() onMessage = new EventEmitter<TextMessageBoxEvent>()
+
+  public fb = inject(FormBuilder)
+  public form = this.fb.group({
+    prompt: ['', Validators.required],
+    selectedOption: ['', Validators.required]
+  })
+
+  handleSubmit(){
+    if(this.form.invalid) return false
+
+    const {prompt, selectedOption} = this.form.value;
+
+    this.onMessage.emit({prompt: prompt!, selectedOption: selectedOption!})
+    this.form.reset()
+
+    return true
+  }
+}
--- src/app/presentation/pages/orthography-page/orthography-page.component.ts
+++ src/app/presentation/pages/orthography-page/orthography-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { GptMessagesComponent, MyMessagesComponent, TextMessageBoxComponent, TextMessageBoxFileComponent, TextMessageBoxFileEvent, TypingLoaderComponent } from '@components/index';
+import { Component, signal } from '@angular/core';
+import { GptMessagesComponent, MyMessagesComponent, TextMessageBoxComponent, TextMessageBoxFileComponent, TextMessageBoxFileEvent, TextMessageBoxSelectComponent, TextMessageBoxEvent, TypingLoaderComponent } from '@components/index';
+import { Message } from '@interfaces/messages.interface';
 
 @Component({
   selector: 'app-orthography-page',
@@ -9,16 +10,23 @@ import { GptMessagesComponent, MyMessagesComponent, TextMessageBoxComponent, Tex
     MyMessagesComponent,
     TypingLoaderComponent,
     TextMessageBoxComponent,
-    TextMessageBoxFileComponent
+    TextMessageBoxFileComponent,
+    TextMessageBoxSelectComponent
   ],
   templateUrl: './orthography-page.component.html'
 })
 export default class OrthographyPageComponent {
+  public messages = signal<Message[]>([{text: 'Hola como estas?', isGpt: false}, {text: 'hi soy tu sensei de gpt', isGpt: true}])
+  public isLoanding = signal<boolean>(false)
+   
   handleMessage(message: string){
     console.log('Handled message:', message)
   }
   handleMessageWithFile({prompt, file}: TextMessageBoxFileEvent){
     console.log('Handled message with file:', prompt, file);
   }
-  
-}
+
+  handleMessageWithSelect(event: TextMessageBoxEvent){
+    console.log('Handled message with file:', event);
+  }
+ }
