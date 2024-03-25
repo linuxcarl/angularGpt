654769da5fafb33ab02657d46ef5d6868754f6af
Carlos Ramirez <carlos@extrasistemas.com>
2024-03-24 18:36:27 -0600

    feat: add base to initializar seciont texto to image

--- src/app/presentation/pages/image-generation-page/image-generation-page.component.ts
+++ src/app/presentation/pages/image-generation-page/image-generation-page.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import {
+  GptMessagesComponent,
+  MyMessagesComponent,
+  TypingLoaderComponent,
+  TextMessageBoxComponent,
+} from '@components/index';
+import { Message } from '@interfaces/messages.interface';
+import { OpenAiService } from 'app/presentation/services/openai.services';
 
 @Component({
   selector: 'app-image-generation-page',
   standalone: true,
-  imports: [],
+  imports: [
+    GptMessagesComponent,
+    MyMessagesComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+  ],
   templateUrl: './image-generation-page.component.html',
-  styleUrl: './image-generation-page.component.css'
 })
 export default class ImageGenerationPageComponent {
+  public messages = signal<Message[]>([]);
+  public isLoanding = signal<boolean>(false);
+  public openAiService = inject(OpenAiService);
 
+  handleMessage(message: string) {
+    console.log('Handled message:', message);
+  }
 }
